2f779073fab8a373ca582b221bebd17608a3abb0
DPCLive <contact@dpclive.com>
2020-03-06 16:12:03 +0100

    Modification du fichier sony_dvs8000.ino

--- sony_dvs8000/sony_dvs8000.ino
+++ sony_dvs8000/sony_dvs8000.ino
@@ -32,13 +32,16 @@ void TimerOne_setPeriod(long OCRValue)
  OCR1A = OCRValue;                          // set the counter
 }
 
-void Select_TMP(uint8_t a0)
+void TMP_Select(uint8_t a0)
 {
+  delayMicroseconds(100);
     PORTL = 0x00; // A9..A11
 
 /*
  * Sélection du TMP IC80
- * 
+
+Bus adresse sur la nappe
+
   A7..A11  : 0
   A6: 1
   A5: 0
@@ -46,61 +49,102 @@ void Select_TMP(uint8_t a0)
   A3: 1 (B)74 HC138 numéro IC6
   A2: 1 (A)74 HC138 numéro IC6
   A1: a0 du TMP (sélection du registre)
-  A0: non connecté (on le met toujours à zéro)
+ 
   */  
     if (a0)
     {
-      //  A7  ... A0
-      PORTC = B01011110;
+      //  A8  ... A1
+      PORTC = B00101111;
     }
     else
     {
-      //  A7  ... A0
-      PORTC = B01011100;
+      //  A8  ... A1
+      PORTC = B00101110;
     }
+    delayMicroseconds(100);
 }
 
 
-void TM_SelectStatusWord()
-{
-  digitalWrite(rd_pin, LOW);  // 0
-  digitalWrite(wr_pin, HIGH); // 1
-  
-  delayMicroseconds(1000);
-  
-  Select_TMP(1);
-  
-  delayMicroseconds(1000);
-}
 
-void TMP_WriteCommand()
+void TMP_WriteCommand(uint8_t data)
 {
   digitalWrite(rd_pin, HIGH);
   digitalWrite(wr_pin, LOW);
   
   delayMicroseconds(1000);
   
-  Select_TMP(1);
+  TMP_Select(1);
+
+  PORTA = data;
   
   delayMicroseconds(1000);
+
+  digitalWrite(wr_pin, HIGH);
+
+  delayMicroseconds(1000);
+}
+
+
+void TMP_WriteData(uint8_t data)
+{
+  digitalWrite(rd_pin, HIGH);
+  digitalWrite(wr_pin, LOW);
+  
+  delayMicroseconds(1000);
+  
+  TMP_Select(0);
+
+  PORTA = data;
+  
+  delayMicroseconds(1000);
+
+  digitalWrite(wr_pin, HIGH);
+
+  delayMicroseconds(1000);
+}
+
+uint8_t TMP_ReadData()
+{
+  digitalWrite(rd_pin, LOW);
+  digitalWrite(wr_pin, HIGH);
+  
+  delayMicroseconds(1000);
+  
+  uint8_t data = PINA;
+
+  delayMicroseconds(1000);
+  digitalWrite(rd_pin, HIGH);
+
+  delayMicroseconds(1000);
+  
+  return data;
 }
 
 void setup() {
   // put your setup code here, to run once:
 
-
   DDRC = B11111111;  // 1 == outputs, 0 == input
   DDRL = B11111111;  // 1 == outputs, 0 == input
 
+// Bus de données
   DDRA = 0;  // 1 == outputs, 0 == input
 
-   pinMode(wr_pin, OUTPUT); 
-   pinMode(rd_pin, OUTPUT); 
-   pinMode(reset_pin, OUTPUT);
+// Configuration du bus de contrôle
+  pinMode(wr_pin, OUTPUT); 
+  pinMode(rd_pin, OUTPUT); 
+  pinMode(reset_pin, OUTPUT);
+  pinMode(test_input_pin, INPUT);
 
-   pinMode(test_input_pin, INPUT);
+  // On place le bus de contrôle à un état stable
+  digitalWrite(rd_pin, HIGH);
+  digitalWrite(wr_pin, HIGH);
+  digitalWrite(reset_pin, HIGH);
 
-   digitalWrite(reset_pin, HIGH);
+   delayMicroseconds(1000);
+
+   TMP_WriteCommand(B00000100); // Configure TMP en mode "Encoded Scan Sensor matrix", le reste par défaut
+
+   TMP_WriteCommand(B00100101); // clock divided by 5
 
   // PARAMETRAGE HORLOGE 
    pinMode(blinkPin, OUTPUT);                 // alternately use DDRB = 0xa0;      // turn on the timer ouput pin;
@@ -108,31 +152,33 @@ void setup() {
 
     // open the serial port at 9600 bps:
   Serial.begin(9600);
-
-  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  uint8_t data = 0;
 
-  //digitalWrite(reset_pin, LOW);
-  delay(20);
-  digitalWrite(reset_pin, HIGH);
-  delay(20);
+  TMP_Select(1); // status 
+  data = TMP_ReadData();
+  Serial.println("STATUS");
+  Serial.println(data, BIN);  // print as an ASCII-encoded binary
 
-  TMP_SelectStatusWord();
-
-  Serial.println(PINA, BIN);  // print as an ASCII-encoded binary
-
-  /*if (digitalRead(test_input_pin) == HIGH)
+/*
+  TMP_WriteData(B01000000);
+  TMP_Select(0); // data
+  data = TMP_ReadData();
+   Serial.println("DATA");
+  Serial.println(data, BIN);  // print as an ASCII-encoded binary
+*/
+/*
+  if (digitalRead(test_input_pin) == HIGH)
   {
     Serial.println("5V ------------");
   }
   else
   {
     Serial.println("0V **********");
-  }*/
-  
+  }
+  */
   delay(200);
   
 }
